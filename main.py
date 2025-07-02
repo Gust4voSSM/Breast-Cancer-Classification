915e7342e626423335eeb5e123ba126b47a16900
Gust4voSSM <gugu9santiago@gmail.com>
2025-07-01 22:55:53 -0300

    Add main

---
+++ main.py
@@ -0,0 +1,15 @@
+from sklearn import (
+    datasets,
+    model_selection,
+    tree,
+    naive_bayes,
+    linear_model,
+    neighbors
+)
+import pandas as pd
+
+def main():
+    ds = pd.read_csv("data/wdbc.data", header=None)
+
+if __name__ == "__main__":
+    main()
