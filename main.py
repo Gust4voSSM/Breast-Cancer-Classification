1019af543aacffc72980d9e7cec0fb7bca7f95f9
Gust4voSSM <gugu9santiago@gmail.com>
2025-07-02 00:03:06 -0300

    Divisão do dataset em treino e teste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sklearn import (
-    datasets,
     model_selection,
     tree,
     naive_bayes,
@@ -8,8 +7,32 @@ from sklearn import (
 )
 import pandas as pd
 
+def analisar_dados(data):
+    print('\n' "Numero de exemplos: ", data.shape[0], '\n',
+          '\n' "Por classe:" '\n',
+          data[1].value_counts(),
+          '\n' "//// HEAD ////" '\n',
+          data.head())
+
 def main():
-    ds = pd.read_csv("data/wdbc.data", header=None)
+    print("Importando dataset...")
+    data = pd.read_csv("data/wdbc.data", header=None)
+    analisar_dados(data)
+    
+    # Colunas de atributos
+    X = data.iloc[:, 2:].values
+
+    # Coluna da classe
+    y = data.iloc[:, 1].values
+    y = (y == 'M').astype(int) # Convertendo numérica (binário)
+
+    # Divisão estratificada; 70% treino, 30% teste
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y, test_size=0.3, random_state=42, stratify=y
+    )
+
+    print(f"Tamanho treino: {X_train.shape[0]}")
+    print(f"Tamanho teste: {X_test.shape[0]}")
 
 if __name__ == "__main__":
     main()
